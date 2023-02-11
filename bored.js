5e2728639287b7613179ec1c457a80b541009393
Vitukaa <vitalija.dau@gmail.com>
2023-02-11 14:23:53 +0200

    Add accessibility

--- bored.js
+++ bored.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('#bored-form')
 const typesInputElement = document.querySelector('#categories')
 const submitButton = document.querySelector('#submit-button')
-const priceRange = document.querySelector('#price-range')
+
 
 
 
@@ -16,34 +16,25 @@ function listSelectionOptions(selectElement) {
     }) 
 }
 
+const priceRange = document.querySelector('#price-range')
+const priceOutput = document.querySelector('.price-output')
+const accessibilityRange = document.querySelector('#accessibility-range')
+const accessibilityOutput = document.querySelector('.accessibility-output')
 
-
-function addRangeInput(rangeInput, min, max, step) {
-    const rangeOutput = document.querySelector('.range-output')
+function addRangeInput(rangeInput, min, max, step, output) {
+    output
     rangeInput.min = min
     rangeInput.max = max
     rangeInput.step = step
     // rangeInput.value = max/2
     let defaultValue = rangeInput.value
     
-    rangeOutput.textContent = defaultValue
-    console.log(rangeInput.value)
+    output.textContent = defaultValue
     
     rangeInput.addEventListener('input', (event) => {
-        rangeOutput.textContent = event.target.value
-        console.log(event.target.value)
-        
+        output.textContent = event.target.value
     })
-
 }
-
-
-function getParticipantsCount() {
-    const participantsCount = form.participant.value
-    return participantsCount
-}
-
-
 
 
 
@@ -65,8 +56,13 @@ activityPrice.classList.add('activity-price')
 const activityParticipants = document.createElement('p')
 activityParticipants.classList.add('activity-participants')
 
+const activityAccessibility = document.createElement('p')
+activityAccessibility.classList.add('activity-accessibility')
+
 const noActivityError = document.createElement('p')
 noActivityError.classList.add('no-activity-error')
+
+
 
 function removeAllChildren(parent) {
     while (parent.firstChild) {
@@ -77,29 +73,52 @@ function removeAllChildren(parent) {
 async function getActivityByParameters() {
     removeAllChildren(activityWrapper)
 
+    let selectedParticipant = form.participant.value
     let selectedType = typesInputElement.value
-    let selectedPrice = priceRange.value/10.0
+    let selectedPrice = priceRange.value / 10.0
+    let selectedAccessibility = accessibilityRange.value / 10.0
 
-    const res = await fetch(`https://www.boredapi.com/api/activity?type=${selectedType}&participants=${getParticipantsCount()}&maxprice=${selectedPrice}`)
+    parametersArr = [
+        ['type', selectedType],
+        ['participants', selectedParticipant],
+        ['maxaccessibility', selectedAccessibility],
+        ['maxprice', selectedPrice]
+    ]
+
+    let parameters = parametersArr.reduce((result, parameterArr, index, arr) => {
+        if (index === arr.length - 1) {
+            return result + `${parameterArr[0]}=${parameterArr[1]}`
+        }
+       return result + `${parameterArr[0]}=${parameterArr[1]}&`
+    }, '')
+    console.log(parameters)
+
+//    console.log(parametersArr.reduce((result, parameterArr) => result + `${parameterArr[0]}=${parameterArr[1]}&`, ''))
+//    console.log(parametersArr.map(parameter => `${parameter[0]}=${parameter[1]}`).join('&'))
+   
+    
+
+
+    const res = await fetch(`https://www.boredapi.com/api/activity?${parameters}`)
     const data = await res.json()
 
     form.after(activityWrapper)
     
     if (!data.hasOwnProperty('error')) {
         
-        let price = data.price*10
+        let price = data.price * 10
+        let accessibility = data.accessibility * 10
+
 
         activityType.textContent = `Type: ${data.type}`
-        console.log(data)
         activityDescription.textContent = `Activity: ${data.activity}`
         activityPrice.textContent = `Price (0-10): ${price}`
-        activityParticipants.textContent = `Participants: ${getParticipantsCount()}`
-        console.log(data)
+        activityParticipants.textContent = `Participants: ${data.participants}`
+        activityAccessibility.textContent = `Accessibility (0-10): ${accessibility}`
 
-        activityWrapper.append(activityType, activityDescription, activityPrice, activityParticipants)
+        activityWrapper.append(activityType, activityDescription, activityPrice, activityParticipants, activityAccessibility)
 
     } else {
-
         noActivityError.textContent = `${data.error}`
         activityWrapper.append(noActivityError)
     }
@@ -110,13 +129,12 @@ async function getActivityByParameters() {
 
 function boredAPI() {
     listSelectionOptions(typesInputElement)
-    addRangeInput(priceRange, 0, 10, 0.5)
+    addRangeInput(priceRange, 0, 10, 0.5, priceOutput)
+    addRangeInput(accessibilityRange, 0, 10, 0.5, accessibilityOutput)
 
     form.addEventListener('submit', async (event) => {
         event.preventDefault()
-        getParticipantsCount()
         await getActivityByParameters()
-        // form.reset()
     })
 
 }
